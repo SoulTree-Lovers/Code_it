e1a70eb4405446470ea5358310a649c331631381
SeungMin Kang <rkdtmdals999@naver.com>
2022-04-14 00:07:00 +0900

    Create CodeIt directory

---
+++ CodeIt/Codeit_example/Algorithm/palindrome.py
@@ -0,0 +1,19 @@
+def is_palindrome(word):
+    new_word = ""
+    for i in range(len(word)):
+        new_word += word[len(word) - i - 1]
+    
+    if new_word == word:
+        return True
+    else:
+        return False
+        
+
+
+
+# 테스트
+print(is_palindrome("racecar"))
+print(is_palindrome("stars"))
+print(is_palindrome("토마토"))
+print(is_palindrome("kayak"))
+print(is_palindrome("hello"))
