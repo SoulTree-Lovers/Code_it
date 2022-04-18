07c616bc038b80cc1f062d45ed4a7e8cf2f57d7b
SeungMin Kang <rkdtmdals999@naver.com>
2022-04-19 00:32:51 +0900

    Update 2022/04/19

--- CodeIt/Algorithm/hanoi.py
+++ CodeIt/Algorithm/hanoi.py
@@ -5,7 +5,7 @@ def move_disk(disk_num, start_peg, end_peg):
 def hanoi(num_disks, start_peg, end_peg):
     # base case: 옮길 원판이 없으면 부분 문제를 나누지 않고 함수를 끝낸다
     if num_disks == 0:
-        return
+        return 
     else:
         other_peg = 6 - start_peg - end_peg
         
@@ -14,7 +14,7 @@ def hanoi(num_disks, start_peg, end_peg):
         
         # 2. 가장 큰 원판을 start_peg에서 end_peg로 이동
         move_disk(num_disks, start_peg, end_peg)
-        
+
         # 3. 나머지 원판들을 other_peg에서 end_peg로 이동
         hanoi(num_disks - 1, other_peg, end_peg)
 
